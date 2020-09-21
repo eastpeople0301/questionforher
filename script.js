a72c30ec2be3e472310fec939dc4ad7acd90b94e
eastpeople0301 <71635855+eastpeople0301@users.noreply.github.com>
2020-09-21 18:15:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var color = prompt('문제 1/3\n내가 제일 좋아하는 색은?');
+  if(color == '하늘색') {
+      var tall = prompt('문제 2/3\n그렇다면 나의 키는?(소수점x)');
+       if(tall == '173')
+         var foot = prompt('문제 3/3 \n 나의 발 사이즈는?');
+            if(foot == 275)  {
+           document.write('다 맞췄네~~~역시 울 쟈기♥♥ 사랑해 ♥');
+          } else {
+              alert('아닌데......실망이야......');
+              } 
+         } else {
+            alert('어떤 놈이랑 헷갈렸냐? 다시 생각해봐');
+         } 
+           
